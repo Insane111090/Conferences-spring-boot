93358a1dfc5faf2443f103a81b0150c1d96f1ae9
Andrei Gavrilov <volirvag132005@bk.ru>
2020-05-31 01:10:33 +0300

    Added UserDTO entity and abstraction

--- src/main/java/com/conferences/service/UserAlreadyHasThisConferenceException.java
+++ src/main/java/com/conferences/service/UserAlreadyHasThisConferenceException.java
@@ -2,6 +2,6 @@ package com.conferences.service;
 
 public class UserAlreadyHasThisConferenceException extends RuntimeException{
     public UserAlreadyHasThisConferenceException(){
-        super("This User already has this Conference with the same Role");
+        super("This User already has this Conference");
     }
 }
